3a655aec6f1d57691dcc4d0618538d06ea39774d
Hawl88 <271202589+Hawl88@users.noreply.github.com>
2026-04-01 01:56:48 +0900

    first commit

---
+++ app/circles/circles.ts
@@ -0,0 +1,32 @@
+export interface circleInfoInterface {
+  circleName: string;
+  shortDescription: string;
+  Image: string;
+}
+
+export const circleInfos: circleInfoInterface[] = [
+  {
+    circleName: "ラーメン同好会",
+    shortDescription: "N君が好きすぎて始めた同好会です",
+    Image:
+      "https://images.pexels.com/photos/36752540/pexels-photo-36752540.jpeg",
+  },
+  {
+    circleName: "肉体改造サークル",
+    shortDescription: "プロテイン必須",
+    Image:
+      "https://images.pexels.com/photos/36752540/pexels-photo-36752540.jpeg",
+  },
+  {
+    circleName: "音ゲーサークル",
+    shortDescription: "N君が好きすぎて始めた同好会です",
+    Image:
+      "https://images.pexels.com/photos/36752540/pexels-photo-36752540.jpeg",
+  },
+  {
+    circleName: "ゲームサークル",
+    shortDescription: "ゲームが好きな人向けのサークルです",
+    Image:
+      "https://images.pexels.com/photos/36752540/pexels-photo-36752540.jpeg",
+  },
+];
---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+export const dynamic = "error";
+export const revalidate = false;
+
+export default function Home() {
+  return <></>;
+}
