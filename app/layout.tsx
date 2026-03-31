03168e092d9b56463378ab9805747bf43aeacd69
gdgdrs <272590152+gdgdrs@users.noreply.github.com>
2026-03-31 19:18:39 +0000

    refactor: lint and format

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 import { Hachi_Maru_Pop } from "next/font/google";
-import type { AppProps } from "next/app";
 
 const font = Hachi_Maru_Pop({
   weight: "400",
@@ -20,18 +19,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`h-full antialiased ` + font.className}>
-      <body className="min-h-full flex flex-col">
+      <body className="flex min-h-full flex-col">
         <header className="navbar bg-base-100 shadow-sm">
           <div className="navbar-start">
             <div className="dropdown">
               <div
                 tabIndex={0}
                 role="button"
-                className="btn btn-ghost btn-circle"
+                className="btn btn-circle btn-ghost"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
+                  className="size-5"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
@@ -46,7 +45,7 @@ export default function RootLayout({
               </div>
               <ul
                 tabIndex={-1}
-                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+                className="dropdown-content menu z-1 mt-3 w-52 menu-sm rounded-box bg-base-100 p-2 shadow-sm"
               >
                 <li>
                   <a>ホーム</a>
@@ -61,15 +60,15 @@ export default function RootLayout({
             </div>
           </div>
           <div className="navbar-center">
-            <a href="/circles" className="btn btn-ghost text-xl">
+            <a href="/circles" className="btn text-xl btn-ghost">
               サークル一覧
             </a>
           </div>
           <div className="navbar-end">
-            <button className="btn btn-ghost btn-circle">
+            <button className="btn btn-circle btn-ghost">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
+                className="size-5"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -82,11 +81,11 @@ export default function RootLayout({
                 />
               </svg>
             </button>
-            <button className="btn btn-ghost btn-circle">
+            <button className="btn btn-circle btn-ghost">
               <div className="indicator">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
+                  className="size-5"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
@@ -98,13 +97,13 @@ export default function RootLayout({
                     d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
                   />
                 </svg>
-                <span className="badge badge-xs badge-primary indicator-item"></span>
+                <span className="indicator-item badge badge-xs badge-primary"></span>
               </div>
             </button>
           </div>
         </header>
         {children}
-        <footer className="footer sm:footer-horizontal bg-gray-800 text-gray-300 p-10 mt-auto">
+        <footer className="mt-auto footer bg-gray-800 p-10 text-gray-300 sm:footer-horizontal">
           <nav>
             <h6 className="footer-title">Menu</h6>
             <a className="link link-hover" href="/circles">
